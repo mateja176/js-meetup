daa2759aaae04f1e74a9b30fe8704739bed04dbe
mateja <ma.petrovic@levi9.com>
2019-08-02 17:04:23 +0200

    feat(app): adds readQuery

--- app/src/Njam.tsx
+++ app/src/Njam.tsx
@@ -1,8 +1,41 @@
+import { gql } from 'apollo-boost';
 import React from 'react';
+import { withApollo, WithApolloClient } from 'react-apollo';
+import { RouteComponentProps } from 'react-router';
 import { Njam as NjamModel } from '../../api/src/models';
 
-const Njam: React.FC<NjamModel> = props => (
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-);
+const query = gql`
+  query {
+    njam {
+      id
+      location
+      description
+      time
+      participants {
+        id
+        name
+        lastname
+      }
+      organizer {
+        id
+        name
+        lastname
+      }
+      ordered
+    }
+  }
+`;
 
-export default Njam;
+interface NjamProps extends NjamModel, RouteComponentProps<{ id: string }> {}
+
+const Njam: React.FC<WithApolloClient<NjamProps>> = ({
+  match: {
+    params: { id },
+  },
+  client,
+}) => {
+  const njam = client.readQuery({ query, variables: { id } });
+
+  return <pre>{JSON.stringify(njam, null, 2)}</pre>;
+};
+export default withApollo(Njam);
