5e673657af4c4a7000dbc4fe9d0cadbd7741b720
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-04 10:17:23 +0200

    feat(app/Loading): centers spinner

--- app/src/components/Loading.tsx
+++ app/src/components/Loading.tsx
@@ -1,8 +1,15 @@
 import { Spin } from 'antd';
 import React from 'react';
+import { Box, Flex } from 'rebass';
 
 export interface LoadingProps {}
 
-const Loading: React.FC<LoadingProps> = () => <Spin size="large" />;
+const Loading: React.FC<LoadingProps> = () => (
+  <Flex justifyContent="center">
+    <Box>
+      <Spin size="large" />
+    </Box>
+  </Flex>
+);
 
 export default Loading;
