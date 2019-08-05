f43f6accea508adb3546b0e6516a6332b065dc93
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-05 23:34:10 +0200

    feat(app/NjamForm): uses hideRequiredMark

--- app/src/Njam/NjamForm.tsx
+++ app/src/Njam/NjamForm.tsx
@@ -23,8 +23,6 @@ const NjamForm: React.FC<NjamFormProps> = ({
   },
   users,
 }) => {
-  const required = !readOnly;
-
   const readOnlyStyle: React.CSSProperties = {
     pointerEvents: readOnly ? 'none' : 'initial',
   };
@@ -40,11 +38,12 @@ const NjamForm: React.FC<NjamFormProps> = ({
       onSubmit={e => {
         e.preventDefault();
       }}
+      hideRequiredMark={readOnly}
     >
       <Form.Item label="Location">
         {form.getFieldDecorator('location', {
           initialValue: location,
-          rules: [{ required }],
+          rules: [{ required: true }],
         })(<Input readOnly={readOnly} />)}
       </Form.Item>
       <Form.Item label="Time">
@@ -52,7 +51,7 @@ const NjamForm: React.FC<NjamFormProps> = ({
           initialValue: moment(time),
           rules: [
             {
-              required,
+              required: true,
               type: 'object',
             },
           ],
