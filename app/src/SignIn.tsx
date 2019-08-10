267389caf734e1cb58586e94a449f329ee526e2e
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-10 17:48:06 +0200

    refactor(app/SignIn): useQuery(usersQuery)

--- app/src/SignIn.tsx
+++ app/src/SignIn.tsx
@@ -1,39 +1,41 @@
+import { useQuery } from '@apollo/react-hooks';
 import { Button, Form, Typography } from 'antd';
 import React from 'react';
-import { Query } from 'react-apollo';
 import { RouteComponentProps } from 'react-router';
 import { usersQuery, UsersQuery } from './apollo';
-import { Err, FormContainer, Loading, UserSelect } from './components';
+import { FormContainer, Loading, Retry, UserSelect } from './components';
 
 export interface SignInProps extends RouteComponentProps {}
 
 const SignIn: React.FC<SignInProps> = ({ history }) => {
   const [userId, setUserId] = React.useState('');
 
+  const usersQueryResult = useQuery<UsersQuery>(usersQuery);
+
+  const { data, loading, error, refetch } = usersQueryResult;
+
   return (
     <FormContainer mt={3}>
       <Typography.Title>Select user</Typography.Title>
       <Form>
-        <Query<UsersQuery> query={usersQuery}>
-          {({ data, error, loading }) => {
-            if (loading) {
-              return <Loading />;
-            } else if (error) {
-              return <Err {...error} />;
-            } else {
-              const { users } = data!;
+        {(() => {
+          if (loading) {
+            return <Loading />;
+          } else if (error) {
+            return <Retry error={error} refetch={refetch} />;
+          } else {
+            const { users } = data!;
 
-              return (
-                <UserSelect
-                  users={users}
-                  onChange={id => {
-                    setUserId(id);
-                  }}
-                />
-              );
-            }
-          }}
-        </Query>
+            return (
+              <UserSelect
+                users={users}
+                onChange={id => {
+                  setUserId(id);
+                }}
+              />
+            );
+          }
+        })()}
         <br />
         <br />
         <Button
