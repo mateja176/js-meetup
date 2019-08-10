80b9a53a8964f23bb182e82aa5481d07f3761057
Milos Tomsik <milostomsik1@gmail.com>
2019-08-11 00:49:01 +0200

    added userid validation to mynjamscount

--- api/src/services/njam.service.ts
+++ api/src/services/njam.service.ts
@@ -89,6 +89,11 @@ export class NjamService {
   }
 
   async getMyNjamsCount(userId: string): Promise<number> {
+    const users: User[] = await this.db.exec(`SELECT TOP 1 * FROM users WHERE id='${userId}'`);
+    if (users.length === 0) {
+      throw new Error(`Could not find user with id: ${userId}`);
+    }
+
     const myNjams: Njam[] = await this.db.exec(`SELECT userId FROM participants LEFT JOIN njams ON participants.njamId = njams.id WHERE userId='${userId}'`);
     return myNjams.length;
   }
