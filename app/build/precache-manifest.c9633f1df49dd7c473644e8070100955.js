27dc896e1654041ec841b7fadae57a13333b1904
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-18 20:46:10 +0200

    feat(app): removes build from .gitignore

---
+++ app/build/precache-manifest.c9633f1df49dd7c473644e8070100955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118777c211608001f258845b3e417c07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff605b704715ae77e6b",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "9f5cae3460fe9d7f37cc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fff605b704715ae77e6b",
+    "url": "/static/js/2.f20e2020.chunk.js"
+  },
+  {
+    "revision": "9f5cae3460fe9d7f37cc",
+    "url": "/static/js/main.6a64f56e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
