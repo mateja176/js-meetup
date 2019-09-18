8d7bd3e5e2d4539da344762dd1f73da053a41b7d
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-18 22:11:13 +0200

    Updates

---
+++ precache-manifest.c258bf41f49837e5cc3e915f58886051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9a02c99cbc78453bdce51c56afacf6",
+    "url": "/njam-njam/index.html"
+  },
+  {
+    "revision": "0dbab7786ee51061b363",
+    "url": "/njam-njam/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "e7c5ec8c646de88bc906",
+    "url": "/njam-njam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0dbab7786ee51061b363",
+    "url": "/njam-njam/static/js/2.f9482a40.chunk.js"
+  },
+  {
+    "revision": "e7c5ec8c646de88bc906",
+    "url": "/njam-njam/static/js/main.71a26dcc.chunk.js"
+  },
+  {
+    "revision": "fae3abffc5f5ecc47984",
+    "url": "/njam-njam/static/js/runtime~main.6e252458.js"
+  }
+]);
