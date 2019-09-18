8902fe8a3c52f7882be80a6c1f707ad49786c663
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-18 21:27:15 +0200

    feat(app): npm run build --prefix app

---
+++ app/build/precache-manifest.6690d15c03aeb761a7771c1acdae082d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b2dcb745a6e59c354908ec25b7ecde",
+    "url": "/njam-njam/index.html"
+  },
+  {
+    "revision": "d9e8383fb5262393d7b3",
+    "url": "/njam-njam/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "ad9b2bd5bf041b9320a3",
+    "url": "/njam-njam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d9e8383fb5262393d7b3",
+    "url": "/njam-njam/static/js/2.95d79139.chunk.js"
+  },
+  {
+    "revision": "ad9b2bd5bf041b9320a3",
+    "url": "/njam-njam/static/js/main.87081018.chunk.js"
+  },
+  {
+    "revision": "fae3abffc5f5ecc47984",
+    "url": "/njam-njam/static/js/runtime~main.6e252458.js"
+  }
+]);
--- app/build/service-worker.js
+++ app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9633f1df49dd7c473644e8070100955.js"
+  "/njam-njam/precache-manifest.6690d15c03aeb761a7771c1acdae082d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/njam-njam/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
