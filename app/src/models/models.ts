8c7aa6890925c25b9a5c2c8d7abc4ded1d127033
mateja <mateja176@gmail.com>
2020-04-17 00:44:40 +0200

    feat(app/models): adds models.ts

---
+++ app/src/models/models.ts
@@ -0,0 +1,29 @@
+import { mapValues } from 'lodash';
+import moment from 'moment';
+import { Njam, Scalars, User } from '../generated/graphql';
+import { toAbsolutePath } from '../utils';
+
+export type Users = User[];
+
+export type Njams = Njam[];
+
+export interface NjamFormValues
+  extends Omit<Njam, 'id' | 'time' | 'organizer' | 'participants'> {
+  time: moment.Moment;
+  organizerId: Scalars['ID'];
+  participantIds: Scalars['ID'][];
+}
+
+export const routeName = {
+  njams: 'njams',
+  createNjam: 'createNjam',
+  users: 'users',
+};
+
+export const routePath = mapValues(routeName, toAbsolutePath);
+
+export const publicRouteName = {
+  signIn: 'signIn',
+};
+
+export const publicRoutePath = mapValues(publicRouteName, toAbsolutePath);
