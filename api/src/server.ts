1ba8eabefcd922d19b49e5ad7e8903867f52e4eb
mateja <mateja176@gmail.com>
2020-04-17 01:07:24 +0200

    feat(api): types alasql as any before creating db
    the return type is any either way
    because of a lack of a proper type signature

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,18 @@
-const alasql = require('alasql');
-import { UserService, NjamService } from './services';
+import alasql from 'alasql';
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 import { Seed } from './seeder';
+import { NjamService, UserService } from './services';
 
-const db = new alasql.Database();
+const db = new (alasql as any).Database();
 Seed(db);
 
 const context = {
-  'userService': new UserService(db),
-  'njamService': new NjamService(db)
-}
+  userService: new UserService(db),
+  njamService: new NjamService(db),
+};
 
-const server = new ApolloServer({schema, context});
+const server = new ApolloServer({ schema, context });
 
 server.listen(4000).then(({ url }) => {
   console.log(`Server ready at ${url}`);
