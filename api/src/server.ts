3245a347fbc9290bf799d9bd0ff1b07bac0cabd8
mateja <mateja176@gmail.com>
2020-04-17 16:21:16 +0200

    feat(api): parametarizes port

--- api/src/server.ts
+++ api/src/server.ts
@@ -14,6 +14,8 @@ const context = {
 
 const server = new ApolloServer({ schema, context });
 
-server.listen(4000).then(({ url }) => {
+const port = process.env.PORT || 4000;
+
+server.listen(port).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
